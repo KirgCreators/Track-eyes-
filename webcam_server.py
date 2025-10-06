9258c13f42e7a53177cb6351abd29479c9e07f95
KirgCreators <futuremakers.kirg@gmail.com>
2025-10-06 14:31:16 +0800

    webcam_server.py

--- webcam_server.py
+++ webcam_server.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-camera = cv2.VideoCapture(2)  # 0 = webcam default
+camera = cv2.VideoCapture(0)  # 0 = webcam default
 
 def generate_frames():
     while True:
